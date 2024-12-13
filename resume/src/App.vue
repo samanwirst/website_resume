3fdd6cbbe19f9bf8f781e4ae5f9ce8ae41ffe49d
samanwirst <muhammadievsamandar2@gmail.com>
2024-12-13 23:04:26 +0500

    first commit

---
+++ resume/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import PersonalInformation from './components/PersonalInformation.vue';
+</script>
+
+<template>
+    <PersonalInformation />
+</template>
+
+<style scoped>
+
+</style>
